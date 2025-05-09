cdf7b15ab896dac425ad7b0195799d7b1db4d9f9
Yashasvi Yadav <yashasviyadav@IN-FVFGK2TTQ05N.local>
2025-05-10 04:15:34 +0530

    combined initial commit for ui and backend

---
+++ urlshortener/src/main/java/com/example/urlshortener/ExceptionHandler/UrlShortenerException.java
@@ -0,0 +1,9 @@
+package com.example.urlshortener.ExceptionHandler;
+
+
+public class UrlShortenerException extends RuntimeException {
+    public UrlShortenerException(String message) {
+        super(message);
+    }
+}
+
