837cfeabeb25a43ae23c7922e4eaf6c294065b9b
Yashasvi Yadav <yashasviyadav@IN-FVFGK2TTQ05N.local>
2025-05-14 01:30:52 +0530

    moved dotenv loading for system logic to dotenvConfig.java for testenv only

--- urlshortener/src/main/java/com/example/urlshortener/UrlshortenerApplication.java
+++ urlshortener/src/main/java/com/example/urlshortener/UrlshortenerApplication.java
@@ -1,6 +1,5 @@
 package com.example.urlshortener;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,12 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UrlshortenerApplication {
 
 	public static void main(String[] args) {
-		// since i m using 3rd party .env support, i need to load the .env file in the system (just like we do export var=value) same this does
-		// then the loaded variables are used by the application.properties
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry ->
-				System.setProperty(entry.getKey(), entry.getValue())
-		);
 		SpringApplication.run(UrlshortenerApplication.class, args);
 	}
 
